2d4fe63cd1e5966fcd05316f8c37d079bfd96491
Kadir Erman <kadirermantr@gmail.com>
2021-08-17 15:04:15 +0300

    yorum satırı eklendi

--- answer.php
+++ answer.php
@@ -9,18 +9,18 @@ $guess = "1470"; // Tahmin edilen değer
 
 $bmrScore = 0;
 
-if ($gender == "male") {
+if ($gender == "male") { // gender değeri male ise
     $bmrScore = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
-} else {
+} else { // gender değeri male değilse
     $bmrScore = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
 }
 
 echo "BMR: $bmrScore\n<br />";
 
-if ($bmrScore < $guess) {
+if ($bmrScore < $guess) { // bmrScore değeri, guess değerinden düşükse
     echo "Tahmin değerinden düşük";
-} elseif ($bmrScore > $guess) {
+} elseif ($bmrScore > $guess) { // bmrScore değeri, guess değerinden büyükse
     echo "Tahmin değerinden büyük";
-} else {
+} else { // hiçbir şarta uymazsa
     echo "Tahmin değerine eşit";
 }
