2855e3a25deec01a07666a70930edc962d67cdeb
Kadir Erman <kadirermantr@gmail.com>
2021-08-19 16:02:58 +0300

    gereksiz kodlar kaldırıldı

--- answer.php
+++ answer.php
@@ -7,8 +7,6 @@ $age = 25; // yaş (sene)
 
 $guess = "1470"; // Tahmin edilen değer
 
-$bmrScore = 0;
-
 if ($gender == "male") { // gender değeri male ise
     $bmrScore = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
 } else { // gender değeri male değilse
