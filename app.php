adac8435a95b46d16ae1b1571239063213ac5b5d
Kadir Erman <kadirermantr@gmail.com>
2021-08-21 10:23:24 +0300

    dosyalar istenen hale getirildi

--- app.php
+++ app.php
@@ -34,3 +34,19 @@ $guess = "1470"; // Tahmin edilen değer
  * 
  * yazması gerekiyor.
  */
+ 
+if ($gender == "male") { // gender değeri male ise
+    $bmrScore = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
+} else { // gender değeri male değilse
+    $bmrScore = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
+}
+
+echo "BMR: $bmrScore\n<br />";
+
+if ($bmrScore < $guess) { // bmrScore değeri, guess değerinden düşükse
+    echo "Tahmin değerinden düşük";
+} elseif ($bmrScore > $guess) { // bmrScore değeri, guess değerinden büyükse
+    echo "Tahmin değerinden büyük";
+} else { // hiçbir şarta uymazsa
+    echo "Tahmin değerine eşit";
+}
